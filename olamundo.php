718809851ed301ab4c4031457709747d4720d459
Byanca Matos <122841376+ByancaMatos01@users.noreply.github.com>
2023-01-27 18:03:50 -0300

    Add files via upload

---
+++ olamundo.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+<style>
+h2{
+    color: blue;
+    text-shadow: 1px 1px 1px black;
+}
+</style>
+
+</head>
+<body>
+    <h1>titulo</h1>
+    <?php
+    echo"<h2>Olá <br> Mundo</h2>";
+    ?>
+
+</body>
+</html>
